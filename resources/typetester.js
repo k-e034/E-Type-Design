f3b0d3fb5315eb404ae2cb1dd0fe23303eecebd4
mosh <83746641+k-e034@users.noreply.github.com>
2022-02-10 12:24:43 +0900

    Update typetester.js

--- resources/typetester.js
+++ resources/typetester.js
@@ -1,30 +1,19 @@
-// フォントサイズ変更
-$('.typeface__fontsize').on('input', function() {
+$('.typeface_fontsize').on('input', function() {
   const fontSize = $(this).val();
   console.log(fontSize);
-  $(this).nextAll('textarea').css('font-size', fontSize + 'px') 
-// ↑場所によってtextareaに当たるように変更する
+  $(this).nextAll('textarea').css('font-size', fontSize + 'px')
 });
 // font-weightとfont-styleでスタイルを変更
-$('#goodday .typeface__select').change(function() {
+$('#goodday .typeface_select').change(function() {
   var property = $(this).find('option:selected').val();
   var weightStyle = property.split('-');
   $('#goodday textarea').css({'font-weight':weightStyle[0], 'font-style':weightStyle[1]})
 });
-// --dmsansのOpenType機能を変更
-$('#goodday .typeface__check').click(function() {
-  var checked = $('#goodday .typeface__check:checked').map(function() {
+$('#goodday .typeface_check').click(function() {
+  var checked = $('#goodday .typeface_check:checked').map(function() {
     return $(this).val();
   }).get();
   features = checked.join('\', \'');
   features = '\'' + features + '\'';
   $('#goodday textarea').css('font-feature-settings', features)
-});
-
-// スライダーでバリアブルフォントを調整
-$('#gdv .typeface__slider').on('input', function() {
-  const weight = $(this).val();
-// 複数軸の場合, const italic = $(#italic).val();のようにそれぞれ定義する
-  $('#gdv textarea').css('font-variation-settings', `'wght' ${weight}`)
-// 複数軸の場合、$('#modern textarea').css('font-variation-settings', `'wght' ${weight}, 'ital' ${italic}`) のように書く
 });

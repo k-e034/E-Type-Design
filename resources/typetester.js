f3d00aa783b04613f8b09f40de019b73cb4f7d73
mosh <83746641+k-e034@users.noreply.github.com>
2022-03-04 13:29:57 +0900

    Update typetester.js

--- resources/typetester.js
+++ resources/typetester.js
@@ -1,13 +1,13 @@
-$('.typeface_fontsize').on('input', function() {
+$('.typeface__fontsize').on('input', function() {
   var fontSize = $(this).val();
   $('.text').css('font-size', fontSize + 'px')
   $('.display').html(fontSize + 'px');
 });
-$('#goodday .typeface_select').change(function() {
+$('#goodday .typeface__select').change(function() {
   var style = $(this).find('option:selected').val();
   $('#goodday textarea').css('font-family',style)});
-$('#goodday .typeface_check').click(function() {
-  var checked = $('#goodday .typeface_check:checked').map(function() {
+$('#goodday .typeface__check').click(function() {
+  var checked = $('#goodday .typeface__check:checked').map(function() {
     return $(this).val();
   }).get();
   features = checked.join('\', \'');
